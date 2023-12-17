ae9cce6a97217843ca8304baf0942e04c25f9ddb
Ubuntu <disi@lab000000.qz5vgk4aljhuxhuqsvmkhavw5e.ax.internal.cloudapp.net>
2023-12-17 14:57:57 +0000

    smallfix

--- scripts/run_tipx_domainnet.sh
+++ scripts/run_tipx_domainnet.sh
@@ -5,16 +5,18 @@ batchSize=2
 inferenceSteps=20
 visualBackbone=RN50
 # encode standard dataset
-python encode_datasets.py --dataset $1
+# python encode_datasets.py --dataset $1
 # generate support set
-python generate_sd_sus.py --dataset $1 --num_images $nSupportImages --prompt_shorthand $promptStrategy --huggingface_key $HFKEY
+python generate_sd_sus.py --dataset $1 --num_images $nSupportImages --prompt_shorthand $promptStrategy --huggingface_key $HFKEY --batch_size $batchSize
 # encode dataset features
 python encode_sus_sd.py --dataset $1 --prompt_shorthand $promptStrategy
 # inference
 python generate_text_classifier_weights.py --dataset $1
 # benchmarking
 python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble > benchmark_sussd_$1.log
-python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k1  > benchmark_susk1_$1.log
-python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k2  > benchmark_susk2_$1.log
-python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k4  > benchmark_susk4_$1.log
-python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k8  > benchmark_susk8_$1.log
+#python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k1  > benchmark_susk1_$1.log
+#python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k2  > benchmark_susk2_$1.log
+#python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k4  > benchmark_susk4_$1.log
+#python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k8  > benchmark_susk8_$1.log
+
+python tipx_classes.py --dataset domainnet_clipart --backbone RN50 --prompt_shorthand photo --sus_type sd --text_prompt_type ensemble 
