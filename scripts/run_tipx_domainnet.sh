abb05a8bf19d1a76c747b741ed27a22c385f42a9
Ubuntu <disi@lab000000.qz5vgk4aljhuxhuqsvmkhavw5e.ax.internal.cloudapp.net>
2023-12-17 10:01:43 +0000

    minor

--- scripts/run_tipx_domainnet.sh
+++ scripts/run_tipx_domainnet.sh
@@ -15,3 +15,6 @@ python generate_text_classifier_weights.py --dataset $1
 # benchmarking
 python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble > benchmark_sussd_$1.log
 python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k1  > benchmark_susk1_$1.log
+python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k2  > benchmark_susk2_$1.log
+python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k4  > benchmark_susk4_$1.log
+python tipx.py --dataset $1 --backbone $visualBackbone --prompt_shorthand $promptStrategy --sus_type sd --text_prompt_type ensemble --support_set_type k8  > benchmark_susk8_$1.log
